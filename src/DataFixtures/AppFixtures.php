e598126979387fefd71f3a5185d3f18ad44c8a4d
yassinebk <yassinebk23@gmail.com>
2022-05-06 09:17:17 +0100

    finished

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,11 +4,14 @@ namespace App\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Prophecy\Comparator\Factory;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
+
+        $faker =
         // $product = new Product();
         // $manager->persist($product);
 
