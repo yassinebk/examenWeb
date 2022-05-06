f12dad5ddeca91119988c1b1da505bd7014811c0
yassinebk <yassinebk23@gmail.com>
2022-05-06 14:49:59 +0100

    finished

--- src/Form/EtudiantType.php
+++ src/Form/EtudiantType.php
@@ -5,6 +5,7 @@ namespace App\Form;
 use App\Entity\Etudiant;
 use App\Entity\Section;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -15,7 +16,8 @@ class EtudiantType extends AbstractType
         $builder
             ->add('nom')
             ->add('prenom')
-            ->add('section',Section::class)
+            ->add('section')
+            ->add( 'Submit',SubmitType::class,)
         ;
     }
 
