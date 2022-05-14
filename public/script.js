d99abb205dc36c42c03e7ef5799b2aae196fbe66
yassinebk <yassinebk23@gmail.com>
2022-05-14 20:44:20 +0100

    finished

--- public/script.js
+++ public/script.js
@@ -1,1 +1,9 @@
 
+
+const rows = document.querySelectorAll('#row')
+console.log(rows)
+
+rows.forEach((r)=>
+r.addEventListener('click',(event)=>{
+   console.log(event.currentTarget.classList.toggle('highlight'))
+}))
